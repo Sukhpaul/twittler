0af133be90e3bfe1849f2fd375e055ca1032d988
Sukhpaul Nijjar <sukhpaul.nijjar@gmail.com>
2017-07-30 19:41:02 -0700

    Add tweet generator
    +function for timeline - refresh to show latest tweets from users in timeline
    +function for user tweet - show latest user tweet in timeline
    +function for users timeline - add click on username to see that  users timeline, double click to exit

--- assets/jquery/application.js
+++ assets/jquery/application.js
@@ -1,21 +1,54 @@
 $(document).ready(function(){
- var $body = $('section .grid');
- $body.html('');
 
- var index = streams.home.length - 1;
-  while(index >= 0){
-   var tweet = streams.home[index];
-   var $tweet = $('<div></div>');
-   $tweet.text('@' + tweet.user + ': ' + tweet.message);
-   $tweet.appendTo($body);
-   index -= 1;
- }
+function genTweetFromObject(tweet) {
+  var $tweet = $('<div class="tweet"></div>');
+  $($tweet).html('@<a href="#userTimeline" class="showTimeline ' + tweet.user + '">' + tweet.user + '</a>: ' + tweet.message + '<br><span class="createdAt">' + tweet.created_at + '</span>');
+  return $tweet;
+}
 
+streams.users.you = [];
+
+$("#sendTweet").click(function(){
+	$("#newestTweets").html('');
+	var yourTweets = {};
+	yourTweets.user = 'you';
+	yourTweets.message = $("#yourtext").val();
+	yourTweets.created_at = new Date();
+	streams.users.you.push(yourTweets);
+	$(genTweetFromObject(streams.users.shawndrost[streams.users.shawndrost.length - 1])).appendTo("#newestTweets");
+	$(genTweetFromObject(streams.users.sharksforcheap[streams.users.sharksforcheap.length - 1])).appendTo("#newestTweets");
+	$(genTweetFromObject(streams.users.mracus[streams.users.mracus.length - 1])).appendTo("#newestTweets");
+	$(genTweetFromObject(streams.users.douglascalhoun[streams.users.douglascalhoun.length - 1])).appendTo("#newestTweets");
+	$(genTweetFromObject(streams.users.you[streams.users.you.length - 1])).prependTo("#newestTweets");
 });
 
-$(document).ready(function() {
-	$('row .grid').on('click', 'button', function() {
-	$(this).find('tweet').slidedown();
-	});
+$('#genTweetBtn').click(function() {
+  $("#newestTweets").html('');
+  $(genTweetFromObject(streams.users.shawndrost[streams.users.shawndrost.length - 1])).appendTo("#newestTweets");
+  $(genTweetFromObject(streams.users.sharksforcheap[streams.users.sharksforcheap.length - 1])).appendTo("#newestTweets");
+  $(genTweetFromObject(streams.users.mracus[streams.users.mracus.length - 1])).appendTo("#newestTweets");
+  $(genTweetFromObject(streams.users.douglascalhoun[streams.users.douglascalhoun.length - 1])).appendTo("#newestTweets");
+  if(streams.users.you[streams.users.you.length - 1] !== undefined) {
+    $(genTweetFromObject(streams.users.you[streams.users.you.length - 1])).prependTo("#newestTweets");
+  }
+});
+
+$("#newestTweets").on('click', ".showTimeline", function() {
+  $("#userTimeline").html("");
+  $("#userTimeline").removeClass("hidden");
+  var username = $(this).attr('class').split(' ')[1];
+  streams.users[username].forEach(function(tweet) {
+    genTweetFromObject(tweet).prependTo("#userTimeline");
+  });
+  var position = $(this).position().top + 60;
+  console.log(position);
+  $("#userTimeline").css("top", '' + position + 'px');
+});
+
+  $("body").on("dblclick", function() {
+    if($("#userTimeline").attr('class') !== "hidden") {
+      $("#userTimeline").attr("class", "hidden");
+    }
+  });
 
 });
